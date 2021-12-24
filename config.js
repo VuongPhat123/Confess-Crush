1ecb704bdd04d068b29ea190e1fc1ef911383431
VuongPhat123 <96602557+VuongPhat123@users.noreply.github.com>
2021-12-24 09:36:38 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Truc's world",
+    introTitle: 'Chíp Chíp',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
