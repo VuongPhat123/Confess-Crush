f9583a75a0b9923b1ef835cdadeaa079f4a99877
VuongPhat123 <96602557+VuongPhat123@users.noreply.github.com>
2021-12-24 09:51:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
-    titleWeb: "Hồng Ngọc",
-    introTitle: 'cđmm',
+    titleWeb: "Truc's World",
+    introTitle: 'Chíp Chíp',
     introDesc: `có điều bất ngờ nè, đừng có mà xĩu ngang nha `,
     btnIntro: 'hehehe',
     title: 'mày khoái t mà m không nói đúng không :))',
     desc: 'mày khoái t đúng hong ',
     btnYes: 'chứ gì nữa :*',
     btnNo: 'Không nha :3',
-    question: 'ủa tại sao khoái mà k nói',
+    question: 'ủa tại sao khoái tui,
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Tại vì m bị cute',
+    reply: 'Tại vì phatz đẹp trai và dễ thương ',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Giáng sinh vui vẻ và an lành nha bà ',
-    btnAccept: 'Okiiiii lun <3',
+    messDesc: 'Giáng sinh vui vẻ và an lành nha  ',
+    btnAccept: 'Hihi cảm ơn luôn <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
